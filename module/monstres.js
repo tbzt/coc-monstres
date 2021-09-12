88de2b1005fcf3650027b1d13596578a2c552f36
ZigmundKreud <63199460+ZigmundKreud@users.noreply.github.com>
2021-09-12 15:09:10 +0200

    Ajout des compendiums spécifiques

--- module/monstres.js
+++ module/monstres.js
@@ -1,3 +1,15 @@
+import {MonstresActorSheet} from "./actor/actor-sheet.js";
+import {MonstresNpcSheet} from "./actor/npc-sheet.js";
+import {MonstresEncounterSheet} from "./actor/encounter-sheet.js";
+import {MonstresItemSheet} from "./items/item-sheet.js";
+
 Hooks.once("init", async () => {
-    console.debug("Chroniques Oubliées Contemporain - Monstres - Initialization...");
+    console.debug("Monstres - Initialization...");
+
+    Actors.registerSheet("coc-monstres", MonstresActorSheet, {types: ["character"], makeDefault: true, label: "MONSTRES.sheet.actor"});
+    Actors.registerSheet("coc-monstre", MonstresNpcSheet, {types: ["npc"], makeDefault: true, label: "MONSTRES.sheet.npc"});
+    Actors.registerSheet("coc-monstre", MonstresEncounterSheet, {types: ["encounter"], makeDefault: true, label: "MONSTRES.sheet.encounter"});
+    // Register item sheets
+    Items.registerSheet("coc-monstre", MonstresItemSheet, {types: ["item", "trait", "capacity", "profile", "path", "encounterWeapon"], makeDefault: true, label: "MONSTRES.sheet.item"});
+
 });
